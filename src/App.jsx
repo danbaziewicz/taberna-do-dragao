da1f42b25e0598dbe9494f52a7ba646d4f2d34b4
danbaziewicz <danxbazi@gmail.com>
2022-08-26 19:01:05 -0300

    feat: rotas criadas na App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer/Footer';
+import Home from './pages/Home/Home';
+import Pedidos from './pages/Pedidos/Pedidos';
+import Cardapio from './pages/Cardapio/Cardapio';
+import Login from './pages/Login/Login';
 
 function App() {
 
   return (
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pedidos' element={<Pedidos />} />
+          <Route path='/cardapio' element={<Cardapio />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
