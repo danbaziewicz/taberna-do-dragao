354e09e936034b390e1fb5dee2cb575edfbeb3cf
danbaziewicz <danxbazi@gmail.com>
2022-08-30 16:30:41 -0300

    feat: header e footer base estruturados.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import S from './Header.module.css'
 
 const Header = () => {
   return (
-    <header></header>
+    <header className={S.header}>
+      <nav className={S.nav}>
+        <Link className={S.link} to={"/"}>
+          Home
+        </Link>
+        <Link className={S.link} to={"/cardapio"}>
+          Cardápio
+        </Link>
+        <Link className={S.link} to={"/pedidos"}>
+          Pedidos
+        </Link>
+        <Link className={S.link} to={"/login"}>
+          Login
+        </Link>
+        
+      </nav>
+    </header>
   )
 }
 
