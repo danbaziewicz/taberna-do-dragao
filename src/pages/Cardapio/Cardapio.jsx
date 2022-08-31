2b9c0a2d870074cec6c728d0807142e549307d77
lelaranja <leticiajlaranja@gmail.com>
2022-08-31 15:49:59 -0300

    style: edição do card

--- src/pages/Cardapio/Cardapio.jsx
+++ src/pages/Cardapio/Cardapio.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import CardProd from '../../components/CardProd/CardProd'
 
 const Cardapio = () => {
   return (
-    <div>Cardapio II</div>
+    <div>
+      <Button variant="contained" startIcon={<DeleteIcon />}>Deletar</Button>
+      <CardProd></CardProd>
+    </div>
+
   )
 }
 
