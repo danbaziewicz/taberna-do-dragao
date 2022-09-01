57f79c8bb34c5418d31df525031edc981422c89a
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-09-01 04:13:30 -0300

    feat: add section a Home.jsx e alguns elementos style: estilo da section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,13 @@ import S from "./Home.module.css";
 const Home = () => {
   return (
     <div className={S.container}>
-      <h4>Projeto desenvolvido por Squad 8</h4>
-      <li>Danylo Baziewicz</li>
-      <li>Erika Rodrigues</li>
-      <li>Letícia Laranja</li>
-      <li>Luis Eduardo Gonçalves</li>
+      <section className={S.conteudo}>
+        <div>
+          <h1>Desfrute dos melhores drinks</h1>
+          <p>Torne seu dia ainda mais especial, com tudo que a taberna tem para te oferecer</p>
+        </div>
+        <div className={S.circulo}></div>
+      </section>
     </div>
   );
 };
