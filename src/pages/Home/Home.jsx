e9f275339497a9b153412e90dd97cea3c67abddc
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-09-01 02:34:30 -0300

    refactor: footer concluido, edição nas props do input e do button, e inicio de código na home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import S from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={S.container}>
+      <h4>Projeto desenvolvido por Squad 8</h4>
+      <li>Danylo Baziewicz</li>
+      <li>Erika Rodrigues</li>
+      <li>Letícia Laranja</li>
+      <li>Luis Eduardo Gonçalves</li>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
