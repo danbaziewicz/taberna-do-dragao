8d257d56ab5ed702580338b3651c227659e1087f
danbaziewicz <danxbazi@gmail.com>
2022-09-01 22:19:11 -0300

    corrigidas questões de alinhamento modal.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,14 @@
-import { Box, Button, Modal as Modal1, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Modal as Modal1,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import S from "./Modal.module.css";
 import { AiFillCloseCircle } from "react-icons/ai";
+import Label from "../common/Label/Label";
 
 const Modal = () => {
   const [open, setOpen] = useState(false);
@@ -13,7 +20,7 @@ const Modal = () => {
   };
   return (
     <div className={S.divModal}>
-      <Button onClick={handleOpenModal}>Open modal</Button>
+      <Button onClick={handleOpenModal}>NOVO PEDIDO</Button>
       <Modal1
         open={open}
         onClose={handleClose}
@@ -32,9 +39,30 @@ const Modal = () => {
                 CADASTRAR NOVO PEDIDO
               </Typography>
             </div>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <Typography
+              id="modal-modal-description"
+              sx={{ mt: 2 }}
+            ></Typography>
+            <div className={S.container}>
+              <div className={S.divProdutos}>
+                <Label text="NOME DO PRODUTO" />
+                <Label text="R$ 00,00" />
+                <TextField
+                  id="outlined-number"
+                  type="number"
+                  sx={{ width: "10ch" }}
+                />
+              </div>
+              <div className={S.divProdutos}>
+                <Label text="NOME DO PRODUTO" />
+                <Label text="R$ 00,00" />
+                <TextField
+                  id="outlined-number"
+                  type="number"
+                  sx={{ width: "10ch" }}
+                />
+              </div>
+            </div>
             <div className={S.divBtns}>
               <Button onClick={handleClose}>FECHAR</Button>
               <Button onClick={handleClose}>SALVAR</Button>
