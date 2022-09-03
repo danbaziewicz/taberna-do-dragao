b6607669a7344f5f4334c974fb09521aca1520f7
danbaziewicz <danxbazi@gmail.com>
2022-09-02 23:50:10 -0300

    service atualizada com novo link da API.

--- src/Service/Service.jsx
+++ src/Service/Service.jsx
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://apirest-pub.herokuapp.com/",
+  baseURL: "https://taberna-dragao.herokuapp.com",
 });
 
 export const getMenu = async () => {
-    const response = await instance.get('/menu');
-    const json = await response.data.menu;
-    return json;
+  const response = await instance.get("/menu");
+  const json = await response.data.dados;
+  return json;
 };
 
 export const postProduto = async (body) => {
-    const response = await instance.post("/menu", body);
-    const json = await response.data.msg;
-    return json;
+  const response = await instance.post("/menu", body);
+  const json = await response.data.msg;
+  return json;
 };
 
 export const deleteProduto = async (id) => {
-    const response = await instance.delete(`/menu/produto/${id}`);
+  const response = await instance.delete(`/menu/produto/${id}`);
 };
 
 export const editaProduto = async (id, body) => {
-    const response = await instance.put(`/menu/produto/${id}`, body);
+  const response = await instance.put(`/menu/produto/${id}`, body);
 };
