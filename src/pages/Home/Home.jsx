6c84fffdf256951a6a4d3a867492f96cc28b009d
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-09-07 01:53:26 -0300

    style: novas atualizações nos cards e na home em geral

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Button from "../../components/common/Button/Button";
 import S from "./Home.module.css";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import craftbeer from "../../assets/images/craftbeer.svg";
-import { GiMartini, GiWaterDrop, GiBeerBottle, GiHops, GiWheat, GiGrain} from "react-icons/gi";
+import {
+  GiMartini,
+  GiWaterDrop,
+  GiBeerBottle,
+  GiHops,
+  GiWheat,
+  GiGrain,
+} from "react-icons/gi";
 import ActionAreaCard from "../../components/common/Card/Card";
+import tequila from "../../assets/images/tequila.png";
+import gin from "../../assets/images/gin.png";
+import martini from "../../assets/images/martini.png";
+import mojito from "../../assets/images/mojito.png";
 
 const Home = () => {
   return (
@@ -19,22 +30,43 @@ const Home = () => {
             te oferecer
           </p>
           <div>
-          <Link className={S.link} to={"/cardapio"}>
-            <Button
-              texto="Peça já o seu!"
-              style={S.button}
-              icon={<GiMartini size="30px" color="#EF761E" />}
-            /></Link>
+            <Link className={S.link} to={"/cardapio"}>
+              <Button
+                texto="Peça já o seu!"
+                style={S.button}
+                icon={<GiMartini size="30px" color="#EF761E" />}
+              />
+            </Link>
           </div>
         </div>
         <div className={S.circulo}>
-          <p>imagem de um drink</p>
+          <img src={mojito}/>
+          <h2>MOJITO</h2>
         </div>
       </section>
       <section className={S.sectionCards}>
-        <ActionAreaCard/>
-        <ActionAreaCard/>
-        <ActionAreaCard/>
+        <div className={S.tituloCards}><h2>Populares na taberna</h2></div>
+        <div className={S.cards}>
+        <ActionAreaCard
+          valor="3.5"
+          image={tequila}
+          titulo="Tequila"
+          descricao="Bebida alcoólica destilada, que é feito a partir do suco fermentado da planta mexicana de agave-azul, especificamente várias variedades de Agave tequilana Weber. Tequila pode ter entre 31% e 55% de álcool."
+        />
+        <ActionAreaCard
+          valor="4.5"
+          image={gin}
+          titulo="Gin"
+          descricao="É uma bebida destilada à base de cereais. Posteriormente passa por um processo de infusão com zimbro e outras especiarias. Tem teor alcoólico entre 37,5% e 50%, dependendo da marca."
+        />
+        <ActionAreaCard
+          valor="4"
+          image={martini}
+          titulo="Martíni"
+          descricao="É um coquetel feito com gin e vermute seco, mexidos com gelo e coado em uma taça cocktail sem gelo. A sua finalização e decoração tradicional é um 'twist' de casca de limão-siciliano, ou com azeitonas verdes."
+        />
+        </div>
+        
       </section>
       <section className={S.conteudo2}>
         <img src={craftbeer} />
@@ -46,11 +78,12 @@ const Home = () => {
             água, levedura, lúpulo e cevada maltada.
           </p>
           <div className={S.icones}>
-            <GiWaterDrop size="80px" color="#EF761E"/> +
-            <GiGrain size="80px" color="#EF761E"/> +
-            <GiHops size="80px" color="#EF761E"/> +
-            <GiWheat size="80px" color="#EF761E"/> =
-            <GiBeerBottle size="80px" color="#EF761E"/></div>
+            <GiWaterDrop size="80px" color="#EF761E" /> +
+            <GiGrain size="80px" color="#EF761E" /> +
+            <GiHops size="80px" color="#EF761E" /> +
+            <GiWheat size="80px" color="#EF761E" /> =
+            <GiBeerBottle size="80px" color="#EF761E" />
+          </div>
         </div>
       </section>
     </div>
